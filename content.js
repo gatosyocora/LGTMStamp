980b562977c2a7bc7d9c65b3c388f04306df361f
gatosyocora <gatosyocora36@gmail.com>
2020-06-01 19:04:04 +0900

    first commit

---
+++ content.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.flex-justify-end').append("<input type='button' class='btn btn-primary ml-1' value='LGTM' id='lgtm-button'>");
+    const imgurl = "https://user-images.githubusercontent.com/18584961/83374460-a6989d00-a406-11ea-9292-564de1b51152.jpg";
+    $(document).on("click", "#lgtm-button", function(){
+        let text = String($("#new_comment_field").val());
+        if (text) text += '\n';
+        const lgtmText = "![LGTM]("+imgurl+")";
+        $('#new_comment_field').val(text+lgtmText);
+    });
+});
